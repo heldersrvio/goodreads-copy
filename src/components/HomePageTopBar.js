53a32ad998a05dbcf339d7571a17c3881a78167f
Helder Sérvio <heldersrvio@gmail.com>
2020-10-06 16:56:31 -0300

    Finished styling for home page top bar

--- src/components/HomePageTopBar.js
+++ src/components/HomePageTopBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import './styles/HomePageTopBar.css';
 
 const HomePageTopBar = (props) => {
     const [email, setEmail] = useState('');
@@ -9,14 +10,16 @@ const HomePageTopBar = (props) => {
     return (
         <div id= "home-page-top-bar">
             <div id="home-page-top-bar-left">
-                <a id="goodreads-home-link" href="/"><span></span></a>
+                <a id="goodreads-home-link" href="/"><img src="https://www.goodreads.com/assets/home/header_logo-8d96d7078a3d63f9f31d92282fd67cf4.png" alt="Goodreads logo"></img></a>
             </div>
             <div id="home-page-top-bar-right">
                 <form id="signin-form">
                     <div id="form-email">
                         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email address"></input>
-                        <label for="remember-me">Remember me</label>
-                        <input id="remember-me" type="checkbox" value={rememberMe} onChange={() => setRememberMe(!rememberMe)}></input>
+                        <div>
+                            <input id="remember-me" type="checkbox" value={rememberMe} onChange={() => setRememberMe(!rememberMe)}></input>
+                            <label htmlFor="remember-me">Remember me</label>
+                        </div>
                     </div>
                     <div id="form-password">
                         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"></input>
